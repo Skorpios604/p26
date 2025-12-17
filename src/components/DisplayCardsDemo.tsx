c61391c2c76c33ebd687d589df6b608ab204e00e
Skorpios <cosmicray23@hotmail.com>
2025-12-17 11:17:19 -0800

    Add a new link to the 3rd card to go to the actual website.

--- src/components/DisplayCardsDemo.tsx
+++ src/components/DisplayCardsDemo.tsx
@@ -13,6 +13,7 @@ const defaultCards = [
         iconClassName: "text-blue-500",
         titleClassName: "text-gray-200",
         image: cardImage3,
+        link: "https://skorpios604.github.io/E-commerce/",
         className:
             "[grid-area:stack] hover:-translate-y-80 before:absolute before:w-[100%] before:outline-1 before:rounded-xl before:outline-border before:h-[100%] before:content-[''] before:bg-blend-overlay before:bg-background/50 hover:before:opacity-0 before:transition-opacity before:duration-700 before:left-0 before:top-0 before:pointer-events-none",
     },
