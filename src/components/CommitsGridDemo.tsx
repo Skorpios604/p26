472f48a4fcc0270b778d1fd449f59877f8dc52eb
Skorpios <cosmicray23@hotmail.com>
2026-02-05 19:47:44 -0800

    Add orbiting skills comp.

--- src/components/CommitsGridDemo.tsx
+++ src/components/CommitsGridDemo.tsx
@@ -1,14 +1,17 @@
 
 import { CommitsGrid } from "./ui/commits-grid"
+import OrbitingSkills from "./ui/orbiting-skills"
+
 
 import { AppleStyleDock } from "./AppleStyleDock"
 
 const CommitsGridDemo = () => {
     return (
-        <>
+        <div className="flex flex-col items-center justify-center w-full min-h-screen gap-8">
+            <OrbitingSkills />
             <CommitsGrid text="the end" className="max-w-4xl" />
             <AppleStyleDock />
-        </>
+        </div>
     )
 }
 
